f962742be8e60c0b17bb377316e71227ae603388
Dio <srw109065@gmail.com>
2021-12-05 18:23:48 +0800

    限制密碼次數1.1版

--- password.py
+++ password.py
@@ -1,18 +1,18 @@
 pw = "a123456"
-miss = 3
+miss = 4
 miss = int(miss)
-password = input("請輸入密碼: ")
 
-if password == pw:
-	print("登入成功")
-
-while password != pw:
-	print("密碼錯誤，你還有", miss, "次機會輸入")
+while True:
 	password = input("請輸入密碼: ")
 	miss -= 1
 	if password == pw:
 		print("登入成功")
+		break
 
 	elif miss == 0:
 		print("登入失敗")
 		break	
+
+	else:
+		print("密碼錯誤，你還有", miss, "次機會輸入")
+		
